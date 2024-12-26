900b49d23dffc3048d1406cb733c3443c355b4a1
AC-3EE <66760632+AC-3EE@users.noreply.github.com>
2024-12-27 02:45:22 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Fetch MongoDB URI from an environment variable
+MONGO_URI = os.getenv("MONGODB_URI")
+client = MongoClient(MONGO_URI)
+db = client["bluetear_UAV"]
+collection_flight_path = db['flight_path']
+collection_heatmap_data = db['heat_map']
+
+@app.route('/get_path', methods=['GET'])
+def get_path():
+    """Fetch data for markers"""
+    data = list(collection_flight_path.find({}, {'_id': 0}))  # Exclude '_id'
+    return jsonify(data)
+
+@app.route('/get_heatmap_data', methods=['GET'])
+def get_heatmap_data():
+    """Fetch data for the heatmap"""
+    data = list(collection_heatmap_data.find({}, {'_id': 0}))  # Exclude '_id'
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
